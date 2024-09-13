ac1862ad5eacc825aaffa6ee066844859991d457
Nastuxx <popovaaa2006@gmail.com>
2024-09-13 23:00:20 +0500

    Add files via upload

---
+++ Tyuiu.PopovaAA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.PopovaAA.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Настя";
+            int age = 18;
+            Console.WriteLine(" Здравствуйте, меня зовут " + name + " и мне " + age + " лет! ");
+            Console.ReadKey();
+        }
+    }
+}
