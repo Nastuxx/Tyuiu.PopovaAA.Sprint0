17eb3d892b1d910c80a63aea521dbff4e304d62c
Nastuxx <popovaaa2006@gmail.com>
2024-09-14 17:27:06 +0500

    Add files via upload

---
+++ Tyuiu.PopovaAA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.PopovaAA.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.PopovaAA.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вызов класса DataService и метода GetMessage из библиотеки Tyuiu.PopovaAA.Sprint0.Task2.V0.Lib
+            Console.WriteLine(DataService.GetMessage("Настя"));
+            Console.ReadKey();
+        }
+    }
+}
