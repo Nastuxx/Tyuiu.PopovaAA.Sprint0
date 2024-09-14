778fe2235865b2d1f9a34445132adb6561ebf1ab
Nastuxx <popovaaa2006@gmail.com>
2024-09-14 17:28:52 +0500

    Add files via upload

---
+++ DataServiceTest.cs
@@ -0,0 +1,19 @@
+using Tyuiu.PopovaAA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.PopovaAA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            //Область создания методов тестирования, методов из библиотеки
+            var name = "Настя";
+            var res = DataService.GetMessage(name);
+
+            //Вызываем класс Assert и метод AreEqual
+            Assert.AreEqual("Привет, Настя", res);
+        }
+    }
+}
